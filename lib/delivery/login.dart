c57d9b3c4df574bff7715ce25594f9f68826ead0
franciscoestebanfigueroa <franciscoesteban@live.com>
2021-04-26 13:32:14 -0300

    Login con logo

--- lib/delivery/login.dart
+++ lib/delivery/login.dart
@@ -3,14 +3,37 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Material App Bar'),
+        title: Text('Delivery'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Hello World'),
-        ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: -360,
+            left: 0,
+            height: 600,
+            width: size.width,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(600),
+                color: Colors.purple,
+              ),
+            ),
+          ),
+          Positioned(
+            top: size.height / 6,
+            right: (size.width / 2) - 60,
+            child: CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: 60,
+              child: FlutterLogo(
+                size: 60,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
